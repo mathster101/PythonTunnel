91e9e22836fa5203e9d8806c9cceaf51e1f11f1d
Mathew Panicker <mathewsimonpanicker@gmail.com>
2024-09-27 23:55:37 -0500

    fixes to server side case

--- maketunnel.py
+++ maketunnel.py
@@ -3,6 +3,11 @@ from fcntl import ioctl
 import select
 import Neo
 
+#########################################
+ttype = "client"# i change this to client on host B
+CLIENT_IP = "192.168.0.47"# only for client side
+#########################################
+
 def openTun(tunName):
     #huge thanks to https://jvns.ca/blog/2022/09/06/send-network-packets-python-tun-tap/
     tun = open("/dev/net/tun", "r+b", buffering=0)
@@ -16,31 +21,25 @@ def openTun(tunName):
 
 def serverUp():
     comm = Neo.Neo()
-    comm = Neo.Neo()
     comm.start_server()
     comm.get_new_conn()
     return comm
 
 def clientUp():
     comm = Neo.Neo()
-    comm.connect_client(IP="192.168.0.47")
+    comm.connect_client(IP=CLIENT_IP)
     return comm
-
-
-ttype = "client"# i change this to client on host B
-
-
+#####################################################
 
 tun = openTun(b"tun0")
 if ttype == "server":
     tcp = serverUp()
 else:
     tcp = clientUp()
-
 print("sockets are up")
 
 while True:
-    inputs = [tun, tcp.sock]
+    inputs = [tun, tcp.sock, tcp.conn]
     outputs = []
     inputs,outputs,errs = select.select(inputs, outputs, inputs)
     for fd in inputs:
@@ -50,6 +49,12 @@ while True:
             tcp.send_data(data)
             print("sent")
         if fd == tcp.sock:
+            #if client
+            data = tcp.receive_data()
+            print(len(data), "from tcp")
+            tun.write(data)
+        if fd == tcp.conn:
+            #if server
             data = tcp.receive_data()
             print(len(data), "from tcp")
             tun.write(data)
